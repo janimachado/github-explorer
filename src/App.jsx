12621a45c7f3db7b6d45d212c2eb8244d4f56069
janimachado <janicota@hotmail.com>
2021-03-09 15:37:47 -0300

    Configurando arquivos

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import './styles/global.scss';
+
+
+export function App() {
+    return <h1>Hellouuu</h1>
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import { render } from 'react-dom';
+import { App } from './App';
 
 
-function App() {
-    return <h1>Oi</h1>
-}
+render(<App />, document.getElementById('root'))
